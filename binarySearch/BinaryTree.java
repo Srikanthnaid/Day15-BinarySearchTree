749bb5f88a207cab75742886c332a061255dcc88
Srikanthnaid <ksru044gmail.com>
2022-11-26 11:07:54 +0530

    Created binary search tree from figure

--- binarySearch/BinaryTree.java
+++ binarySearch/BinaryTree.java
@@ -1,8 +1,8 @@
 package binarySearch;
 
 public class BinaryTree {
-	
-	//created new node
+
+	// created new node
 	public Node createNewNode(int k) {
 		Node bst = new Node();
 		bst.data = k;
@@ -10,30 +10,35 @@ public class BinaryTree {
 		bst.right = null;
 		return bst;
 	}
-	
-	//insert data to node
+
+	// insert data to node
 	public Node insert(Node root, int val) {
-		
-		if (root == null) {//56 == null condition get false
-			return createNewNode(val);//it will go to node create method and create new node with data 56.
+
+		if (root == null) {// 56 == null condition get false
+			return createNewNode(val);// it will go to node create method and create new node with data 56.
+
 		}
-		if (val < root.data) { //30<56  condition is true.
-			root.left = insert(root.left, val);////30 will come left side to root node
-			
-		} else if (val > root.data) {//70>56 condition is true
-			root.right = insert(root.right, val);//70 will goes to right of root node.
+		if (val < root.data) { // 30<56 condition is true.
+			root.left = insert(root.left, val);//// 30 will come left side to root node
+
+		} else if (val > root.data) {// 70>56 condition is true
+			root.right = insert(root.right, val);// 70 will goes to right of root node.
 		}
 		return root;
 	}
 
+	/** In-Order traversal :- root , left , right **/
 	public void print(Node root) {
 		if (root == null) {
 			return;
 		}
+
 		print(root.left);
-		System.out.println(root.data);
+		System.out.println(root.data + " ");
 		print(root.right);
+
 	}
+
 
 	public static void main(String[] args) {
 		System.out.println("welcome to BTS program");
@@ -42,7 +47,19 @@ public class BinaryTree {
 		root = bst.insert(root, 56);
 		root = bst.insert(root, 30);
 		root = bst.insert(root, 70);
-		bst.print(root); // output-> 30->56->70
+		root = bst.insert(root, 22);
+		root = bst.insert(root, 40);
+		root = bst.insert(root, 60);
+		root = bst.insert(root, 95);
+		root = bst.insert(root, 11);
+		root = bst.insert(root, 65);
+		root = bst.insert(root, 3);
+		root = bst.insert(root, 6);
+		root = bst.insert(root, 16);
+		root = bst.insert(root, 63);
+		root = bst.insert(root, 67);
+		bst.print(root);
 
 	}
+
 }
